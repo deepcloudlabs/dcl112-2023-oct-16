57f85c89df726f5242e76afed4ef554f2648e512
deepcloudlabs <deepcloudlabs@gmail.com>
2023-10-17 10:54:30 +0300

    Update Vehicle.h

--- practice-session01/Vehicle.h
+++ practice-session01/Vehicle.h
@@ -3,12 +3,15 @@
 
 
 class Vehicle {
-public:
+private:
     double load;
     const double capacity;
+public:
     Vehicle(double capacity) : capacity(capacity), load(0.0){}
     double getLoad() const { return this->load;}
     double getCapacity() const { return this->capacity;}
+    bool addBox(double weight);
+    bool removeBox(double weight);
 };
 
 #endif //PRACTICE_SESSION01_VEHICLE_H
