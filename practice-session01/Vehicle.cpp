df434de0685ad8ac2035f0fedccb1f413ef99801
deepcloudlabs <deepcloudlabs@gmail.com>
2023-10-17 10:54:34 +0300

    Update Vehicle.cpp

--- practice-session01/Vehicle.cpp
+++ practice-session01/Vehicle.cpp
@@ -1,5 +1,20 @@
-//
-// Created by dcl on 10/17/2023.
-//
 
 #include "Vehicle.h"
+
+bool Vehicle::addBox(double weight) {
+    // validation rule
+    if (weight <= 0) return false;
+    // business rule
+    if (weight + this->load > this->capacity) return false;
+    this->load += weight;
+    return true;
+}
+
+bool Vehicle::removeBox(double weight) {
+    // validation rule
+    if (weight <= 0) return false;
+    // validation rule
+    if (weight > this->load) return false;
+    this->load -= weight;
+    return true;
+}
